f786a124636998f1807e1dc29defacadc9de4f06
Tommaso Chiti <chitilivorno@gmail.com>
2022-06-05 14:45:58 +0200

    Execution time.

--- commands/execute.js
+++ commands/execute.js
@@ -14,22 +14,24 @@ module.exports = {
 	async execute(interaction) {
 		const input = await interaction.options.getString('input');
 		try {
+			const startup = new Date();
 			const { stdout } = await exec(input);
+			const ready = new Date();
+			const diff = ready.getTime() - startup.getTime();
 			const commandEmbed = new MessageEmbed()
 				.setColor('#55ff55')
-				.setAuthor({ name: 'Some name', iconURL: 'https://i.imgur.com/AfFp7pu.png', url: 'https://discord.js.org' })
 				.setThumbnail('https://raw.githubusercontent.com/classy-giraffe/SafeBox/main/assets/img/tick.png')
 				.addFields(
 					{ name: 'Input', value: codeBlock(input) },
 					{ name: 'Output', value: codeBlock(stdout) },
 				)
-				.setTimestamp();
+				.setTimestamp()
+				.setFooter({ text: `It took ${diff}ms` });
 			await interaction.reply({ embeds: [ commandEmbed ] });
 		}
 		catch (error) {
 			const commandEmbed = new MessageEmbed()
 				.setColor('#ff5555')
-				.setAuthor({ name: 'Some name', iconURL: 'https://i.imgur.com/AfFp7pu.png', url: 'https://discord.js.org' })
 				.setThumbnail('https://raw.githubusercontent.com/classy-giraffe/SafeBox/main/assets/img/error.png')
 				.addFields(
 					{ name: 'Input', value: codeBlock(input) },
