680422e2b55ab6905548ae2d0451b9af8b40bfc8
Tommaso Chiti <chitilivorno@gmail.com>
2022-06-06 12:07:02 +0200

    RAM Usage.

--- commands/status.js
+++ commands/status.js
@@ -1,17 +1,20 @@
 const { SlashCommandBuilder, codeBlock } = require('@discordjs/builders');
-const { MessageEmbed } = require('discord.js');
+const { MessageEmbed, version } = require('discord.js');
 
 module.exports = {
 	data: new SlashCommandBuilder()
 		.setName('status')
-		.setDescription('Print informations about the bot'),
+		.setDescription('Print informations about the bot.'),
 	async execute(interaction) {
+		const memory = Math.round(process.memoryUsage().heapUsed / 1024 / 1024 * 100) / 100;
 		const status = new MessageEmbed()
 			.setColor('#55ff55')
 			.setThumbnail('https://raw.githubusercontent.com/classy-giraffe/SafeBox/main/assets/img/tick.png')
 			.addFields(
-				{ name: 'Node dependencies:', value: codeBlock(process.versions) },
-				{ name: 'Node.js version:', value: codeBlock(process.versions.node) },
+				{ name: 'Platform', value: codeBlock(process.platform) },
+				{ name: 'Node.js version', value: codeBlock(process.versions.node) },
+				{ name: 'Discord.js version', value: codeBlock(version) },
+				{ name: 'Used memory', value: codeBlock(`${memory} MB`) },
 			)
 			.setTimestamp();
 		await interaction.reply({ embeds: [ status ] });
