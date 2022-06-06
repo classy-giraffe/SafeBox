33ac59a4925bf44c067f0970892da60ddfa2146a
Tommaso Chiti <chitilivorno@gmail.com>
2022-06-06 11:37:11 +0200

    Fix status command.

--- commands/status.js
+++ commands/status.js
@@ -3,10 +3,10 @@ const { MessageEmbed } = require('discord.js');
 
 module.exports = {
 	data: new SlashCommandBuilder()
-		.setName('Status')
+		.setName('status')
 		.setDescription('Print informations about the bot'),
 	async execute(interaction) {
-		const commandEmbed = new MessageEmbed()
+		const status = new MessageEmbed()
 			.setColor('#55ff55')
 			.setThumbnail('https://raw.githubusercontent.com/classy-giraffe/SafeBox/main/assets/img/tick.png')
 			.addFields(
@@ -14,6 +14,6 @@ module.exports = {
 				{ name: 'Node.js version:', value: codeBlock(process.versions.node) },
 			)
 			.setTimestamp();
-		await interaction.reply({ embeds: [ commandEmbed ] });
+		await interaction.reply({ embeds: [ status ] });
 	},
 };
