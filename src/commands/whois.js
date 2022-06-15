0607f238d39d5fb71e12e0c44a25cf33e7f85af6
sorcerer_worker <worker.sorcerer@yandex.com>
2022-06-15 17:19:12 -0400

    feat: Add whois command

---
+++ src/commands/whois.js
@@ -0,0 +1,26 @@
+const { SlashCommandBuilder } = require('@discordjs/builders');
+const { MessageEmbed } = require('discord.js');
+
+module.exports = {
+	data: new SlashCommandBuilder()
+		.setName('whois')
+		.setDescription('Shows informaion about yourself or a particular user.')
+		.addUserOption(option =>
+			option.setName('user')
+				.setDescription('The user you want info about.')),
+	async execute(interaction) {
+		chosenMember = await interaction.options.getUser('user') || interaction.user
+		extractBio = `
+**Nickname:** ${chosenMember.username}
+**Discriminator:** ${chosenMember.discriminator}
+**Created At:** ${chosenMember.createdAt}
+**Bot?:** **${chosenMember.bot ? "True" : "False"}${chosenMember.bot ? "" : ", but you never know."}**
+`
+		const info = new MessageEmbed()
+			.setColor('#55ff55')
+			.setTitle('User Info:')
+			.setDescription(extractBio)
+		interaction.reply({ embeds: [info] });
+
+	},
+};
