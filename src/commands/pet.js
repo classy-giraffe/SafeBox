8ffe8566b950572177bbfe2b565aad168fa676cd
Tommaso Chiti <chitilivorno@gmail.com>
2022-07-06 14:16:48 +0200

    Initialize commands.

--- src/commands/pet.js
+++ src/commands/pet.js
@@ -1,10 +1,22 @@
 const { SlashCommandBuilder } = require('@discordjs/builders');
+const { MessageEmbed } = require('discord.js');
 
 module.exports = {
 	data: new SlashCommandBuilder()
-		.setName('pet')
-		.setDescription('Pet somebody!'),
+		.setName('cookie')
+		.setDescription('Give somebody a cookie.')
+		.addUserOption(option =>
+			option
+				.setName('user')
+				.setDescription('The user you wanna give a cookie to.')
+				.setRequired(true),
+		),
 	async execute(interaction) {
-    
-    },
+		const cookie = new MessageEmbed()
+			.setColor('#55ff55')
+			.setTitle('gives a cookie to')
+			.setURL('https://github.com/classy-giraffe')
+			.setFooter({ text: 'That\'s <> cookies now!' });
+		await interaction.reply({ embeds: [ cookie ] });
+	},
 };
