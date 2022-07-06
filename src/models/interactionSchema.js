01e1357a94bb150c9dbe6a66916bdf854d14c987
Tommaso Chiti <chitilivorno@gmail.com>
2022-07-06 19:56:56 +0200

    Schema.

--- src/models/interactionSchema.js
+++ src/models/interactionSchema.js
@@ -1,15 +1,14 @@
 const mongoose = require('mongoose');
 
-const interactionSchema = new mongoose.Schema({
-	authorUserId: { type: String, require: true },
-	authorUserTag: { type: String, require: true },
-	guildId: { type: String, require: true },
-	channelId: { type: String, require: true },
-	id: { type: String, require: true },
-	createdAt: { type: Date, require: true },
-	interaction: { type: Map, of: String },
+const userSchema = new mongoose.Schema({
+	ID: { type: Number, unique: true, require: true },
+	cookies: { type: Number, default: 0 },
+	hugs: { type: Number, default: 0 },
+	pets: { type: Number, default: 0 },
+	slaps: { type: Number, default: 0 },
+	spanks: { type: Number, default: 0 },
 });
 
-const model = mongoose.model('interactionLog', interactionSchema);
+const model = mongoose.model('users', userSchema);
 
 module.exports = model;
