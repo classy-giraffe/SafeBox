ed5266560da65f114a7ed0a570a74b706fa4f52c
Tommaso Chiti <chitilivorno@gmail.com>
2022-08-04 07:52:51 +0200

    Fixes.

---
+++ src/index-2.js
@@ -0,0 +1,53 @@
+require('dotenv').config();
+const fs = require('fs');
+const path = require('path');
+const mongoose = require('mongoose');
+const { Client, Collection, GatewayIntentBits } = require('discord.js');
+const { token } = process.env.DISCORD_TOKEN;
+const client = new Client({
+	intents: [
+		GatewayIntentBits.Guilds,
+		GatewayIntentBits.GuildMessages,
+		GatewayIntentBits.GuildMembers,
+	],
+});
+
+// Importing commands (from ./commands)
+client.commands = new Collection();
+const commandsPath = path.join(__dirname, 'commands');
+const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js'));
+for (const file of commandFiles) {
+	const filePath = path.join(commandsPath, file);
+	const command = require(filePath);
+	client.commands.set(command.data.name, command);
+}
+
+// Importing events (from ./events)
+const eventsPath = path.join(__dirname, 'events');
+const eventFiles = fs.readdirSync(eventsPath).filter(file => file.endsWith('.js'));
+for (const file of eventFiles) {
+	const filePath = path.join(eventsPath, file);
+	const event = require(filePath);
+	if (event.once) {
+		client.once(event.name, (...args) => event.execute(...args));
+	}
+	else {
+		client.on(event.name, (...args) => event.execute(...args));
+	}
+}
+
+// Initializing MongoDB Connection
+const URI = process.env.MONGO_INITDB_URL;
+try {
+	mongoose.connect(URI, {
+		useNewUrlParser: true,
+		useUnifiedTopology: true,
+	});
+	console.log('Connected to MongoDB!');
+}
+catch (err) {
+	console.error(err);
+}
+
+// Login to Discord API
+client.login(token);
--- src/index.js
+++ src/index.js
@@ -1,6 +1,4 @@
 require('dotenv').config();
-const fs = require('fs');
-const path = require('path');
 const mongoose = require('mongoose');
 const { Client, Collection, GatewayIntentBits } = require('discord.js');
 const { token } = process.env.DISCORD_TOKEN;
@@ -12,29 +10,14 @@ const client = new Client({
 	],
 });
 
-// Importing commands (from ./commands)
-client.commands = new Collection();
-const commandsPath = path.join(__dirname, 'commands');
-const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js'));
-for (const file of commandFiles) {
-	const filePath = path.join(commandsPath, file);
-	const command = require(filePath);
-	client.commands.set(command.data.name, command);
-}
+// Handler
+client.aliases = new Collection();
+client.slashCommands = new Collection();
+module.exports = client;
 
-// Importing events (from ./events)
-const eventsPath = path.join(__dirname, 'events');
-const eventFiles = fs.readdirSync(eventsPath).filter(file => file.endsWith('.js'));
-for (const file of eventFiles) {
-	const filePath = path.join(eventsPath, file);
-	const event = require(filePath);
-	if (event.once) {
-		client.once(event.name, (...args) => event.execute(...args));
-	}
-	else {
-		client.on(event.name, (...args) => event.execute(...args));
-	}
-}
+['commands', 'events'].forEach((handler) => {
+	require(`./handlers/${handler}`)(client);
+});
 
 // Initializing MongoDB Connection
 const URI = process.env.MONGO_INITDB_URL;
